8953d08dc845823342237b74d3516554e0052f3b
Katuula Joel <joel@tunga.io>
2021-07-29 16:43:58 +0300

    fix tests

--- src/layouts/__tests__/DashboardLayout.js
+++ src/layouts/__tests__/DashboardLayout.js
@@ -2,13 +2,25 @@ import React from "react";
 import renderer from "react-test-renderer";
 import { BrowserRouter as Router } from "react-router-dom";
 import DashboardLayout from "../DashboardLayout";
+import { Provider } from "react-redux";
+import configureStore from "redux-mock-store";
+import thunk from "redux-thunk";
+
+const middlewares = [thunk];
+
+const mockAppStore = (state) => {
+  const mockStore = configureStore(middlewares);
+  return mockStore(state);
+};
 
 describe("Auth layout test", () => {
   it("Snapshot test for Login component", () => {
     const tree = renderer
       .create(
         <Router>
-          <DashboardLayout />
+          <Provider store={mockAppStore()}>
+            <DashboardLayout />
+          </Provider>
         </Router>
       )
       .toJSON();
--- src/pages/Dashboard/dashboard/__tests__/dashboard.js
+++ src/pages/Dashboard/dashboard/__tests__/dashboard.js
@@ -1,10 +1,29 @@
 import React from "react";
 import renderer from "react-test-renderer";
 import Dashboard from "../Dashboard";
+import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import configureStore from "redux-mock-store";
+import thunk from "redux-thunk";
+
+const middlewares = [thunk];
+
+const mockAppStore = (state) => {
+  const mockStore = configureStore(middlewares);
+  return mockStore(state);
+};
 
 describe("Dashboard test", () => {
   it("Should match snapshot test", () => {
-    const tree = renderer.create(<Dashboard />).toJSON();
+    const tree = renderer
+      .create(
+        <BrowserRouter>
+          <Provider store={mockAppStore()}>
+            <Dashboard />
+          </Provider>
+        </BrowserRouter>
+      )
+      .toJSON();
     expect(tree).toMatchSnapshot();
   });
 });
