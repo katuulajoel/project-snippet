5486069d9d349d854461bb50c51ddac154e459ca
sumohammed <sumohammed@hotmail.com>
2021-10-04 08:58:18 +0000

    Renamed actions imports to actionTypes

--- src/reducers/InvitesReducer.js
+++ src/reducers/InvitesReducer.js
@@ -1,10 +1,10 @@
-import * as UserActions from "../actions/InvitesActions";
+import * as actionTypes from "../actions/utils/ActionTypes";
 
 function invites(state = {}, action) {
   switch (action.type) {
-    case UserActions.GET_PENDING_INVITES_SUCCESS:
+    case actionTypes.GET_PENDING_INVITES_SUCCESS:
       return { ...(action.payload || {}) };
-    case UserActions.GET_MORE_PENDING_INVITES_SUCCESS:
+    case actionTypes.GET_MORE_PENDING_INVITES_SUCCESS:
       return { invites: [...state.invites, ...action.items] };
     default:
       return state;
