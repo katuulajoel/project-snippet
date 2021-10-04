7b81a97a91553e2e10dbedc6ee27b708eab7b865
sumohammed <sumohammed@hotmail.com>
2021-10-04 08:41:23 +0000

    Added Switch react router comp as sub router

--- src/pages/Dashboard/settings/index.js
+++ src/pages/Dashboard/settings/index.js
@@ -1,6 +1,18 @@
 import React from "react";
+import { Switch, Route } from "react-router-dom";
 import DashboardLayout from "../../../layouts/DashboardLayout";
+import Invite from "./Invite";
+import PendingInvite from "./Invite/views/PendingInvites";
 
 export default function Settings() {
-  return <DashboardLayout>{/* <h3>Payments</h3> */}</DashboardLayout>;
+  return (
+    <DashboardLayout>
+      <div className={`content-card settings-card settings-container clearfix`}>
+        <Switch>
+          <Route path="/settings/invite" component={Invite} />
+          <Route path="/settings/invites/pending" component={PendingInvite} />
+        </Switch>
+      </div>
+    </DashboardLayout>
+  );
 }
