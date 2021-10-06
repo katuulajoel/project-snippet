8b037b511b6396aafabfafd6be38c9772ef0cbeb
sumohammed <sumohammed@hotmail.com>
2021-10-06 16:10:59 +0000

    Updated actionType names

--- src/reducers/InvitesReducer.js
+++ src/reducers/InvitesReducer.js
@@ -2,10 +2,15 @@ import * as actionTypes from "../actions/utils/ActionTypes";
 
 function invites(state = {}, action) {
   switch (action.type) {
-    case actionTypes.GET_PENDING_INVITES_SUCCESS:
+    case actionTypes.SET_PENDING_INVITES:
       return { ...(action.payload || {}) };
-    case actionTypes.GET_MORE_PENDING_INVITES_SUCCESS:
-      return { invites: [...state.invites, ...action.items] };
+    case actionTypes.SET_MORE_PENDING_INVITES:
+      return { ...state, invites: [...state.invites, ...action.items] };
+    case actionTypes.DELETE_PENDING_INVITE:
+      return {
+        ...state,
+        results: [...state.results.filter((item) => action.id !== item.id)],
+      };
     default:
       return state;
   }
