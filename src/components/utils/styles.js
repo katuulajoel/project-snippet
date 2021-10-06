f2861e85200649b0a79a5f1c105157b360ae0e78
sumohammed <sumohammed@hotmail.com>
2021-10-06 16:08:49 +0000

    Added ContentSection components

--- src/components/utils/styles.js
+++ src/components/utils/styles.js
@@ -343,3 +343,90 @@ export const StyledTable = styled(Table)`
     }
   }
 `;
+
+export const ContentSection = styled.div`
+  padding-bottom: 60px;
+
+  .label-style {
+    color: #da3451;
+    padding-left: 2px;
+  }
+  .save-container {
+    height: fit-content;
+    display: flex;
+    padding: 0px 40px 40px;
+    position: absolute;
+    bottom: 0;
+    left: 0;
+  }
+
+  .header {
+    font-weight: 600;
+    font-size: 16px;
+    line-height: 24px;
+    color: #151a30;
+    margin-bottom: 24px;
+  }
+
+  form {
+    .control-label {
+      font-weight: 500;
+      font-size: 14px;
+      line-height: 21px;
+      color: #151a30;
+    }
+
+    input,
+    textarea,
+    select {
+      background: #ffffff;
+      border: 1px solid rgba(194, 204, 217, 0.25);
+      box-sizing: border-box;
+      box-shadow: none;
+      border-radius: 4px;
+      margin-bottom: 16px;
+      font-size: 16px;
+      line-height: 24px;
+      color: #3e4857;
+    }
+
+    .tag-input {
+      .input-group {
+        box-shadow: none;
+        input {
+          background: #ffffff;
+          border: 1px solid rgba(194, 204, 217, 0.25);
+          box-sizing: border-box;
+          box-shadow: none;
+          border-radius: 4px;
+          margin-bottom: 16px;
+          font-size: 16px;
+          line-height: 24px;
+          color: #3e4857;
+          &::placeholder {
+            padding: 8px 16px;
+          }
+        }
+      }
+    }
+
+    .save {
+      font-style: normal;
+      font-weight: 600;
+      font-size: 16px;
+      line-height: 20px;
+      text-align: center;
+
+      color: #ffffff;
+      box-shadow: none;
+      border: none;
+
+      background: #da3451;
+      border-radius: 4px;
+
+      &:disabled {
+        opacity: 0.5;
+      }
+    }
+  }
+`;
