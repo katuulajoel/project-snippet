9f344f0da015e701f094f825c3079a90fdf9962d
sumohammed <sumohammed@hotmail.com>
2021-10-08 06:39:29 +0000

    Updated InvitesReducer tests

--- src/reducers/__tests__/InvitesReducer.js
+++ src/reducers/__tests__/InvitesReducer.js
@@ -12,10 +12,98 @@ describe("Invites reducers tests", () => {
     expect(
       reducer(initialState, {
         type: actionTypes.SET_PENDING_INVITES,
-        payload: { invites: [] },
+        payload: { results: [] },
       })
     ).toEqual({
-      invites: [],
+      results: [],
+    });
+  });
+
+  it("handles successfull set data dispatch", () => {
+    expect(
+      reducer(initialState, {
+        type: actionTypes.SET_PENDING_INVITES,
+        payload: {
+          count: 0,
+          next: "",
+          previous: null,
+          results: [
+            {
+              id: 222,
+            },
+          ],
+        },
+      })
+    ).toEqual({
+      count: 0,
+      next: "",
+      previous: null,
+      results: [
+        {
+          id: 222,
+        },
+      ],
+    });
+  });
+
+  it("handles load more pending invite dispatch", () => {
+    const initialState = {
+      count: 0,
+      next: "",
+      previous: null,
+      results: [
+        {
+          id: 222,
+        },
+      ],
+    };
+
+    expect(
+      reducer(initialState, {
+        type: actionTypes.SET_MORE_PENDING_INVITES,
+        payload: [
+          {
+            id: 111,
+          },
+        ],
+      })
+    ).toEqual({
+      count: 0,
+      next: "",
+      previous: null,
+      results: [
+        {
+          id: 222,
+        },
+        {
+          id: 111,
+        },
+      ],
+    });
+  });
+
+  it("handles delete dispatches", () => {
+    const initialState = {
+      count: 0,
+      next: "",
+      previous: null,
+      results: [
+        {
+          id: 222,
+        },
+      ],
+    };
+
+    expect(
+      reducer(initialState, {
+        type: actionTypes.DELETE_PENDING_INVITE,
+        id: 222,
+      })
+    ).toEqual({
+      count: 0,
+      next: "",
+      previous: null,
+      results: [],
     });
   });
 });
