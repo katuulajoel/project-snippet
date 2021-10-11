4e4768fca76b0cbc1891b7a017cac4c07dc0b825
manaraph <manaraph225@gmail.com>
2021-10-11 13:16:48 +0100

    Add tests for table cells component

--- src/pages/Dashboard/tests/index.js
+++ src/pages/Dashboard/tests/index.js
@@ -1,21 +1,15 @@
-import React from "react";
-import { useSelector } from "react-redux";
-import { toggleRightNav } from "../../../actions/UtilityActions";
-import DashboardLayout from "../../../layouts/DashboardLayout";
-import TestsContainer from "./TestContainer";
-import { deleteResult, fetchResults, updateResult } from "../../../actions/TestResultsActions";
+import React from 'react';
+import { useSelector } from 'react-redux';
+import { toggleRightNav } from '../../../actions/UtilityActions';
+import DashboardLayout from '../../../layouts/DashboardLayout';
+import TestsContainer from './TestContainer';
 
 const TestsPage = () => {
   const testResults = useSelector((state) => state.TestResults);
-  const testResultsActions = { deleteResult, fetchResults, updateResult };
 
   return (
     <DashboardLayout>
-      <TestsContainer
-        TestResults={testResults}
-        collapseRightNav={toggleRightNav}
-        TestResultsActions={testResultsActions}
-      />
+      <TestsContainer TestResults={testResults} collapseRightNav={toggleRightNav} />
     </DashboardLayout>
   );
 };
