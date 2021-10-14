219868aac6541c930e671b6c202c17031e9e0387
sumohammed <sumohammed@hotmail.com>
2021-10-14 03:51:07 +0000

    Added ModalContainer component

---
+++ src/components/ModalContainer.jsx
@@ -0,0 +1,55 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { Modal } from "react-bootstrap";
+// import { confirmable, createConfirmation } from "react-confirm";
+import Button from "./Button";
+
+const ModalContainer = (props) => {
+  const {
+    proceedLabel,
+    // cancelLabel,
+    title,
+    confirmation,
+    show,
+    proceed,
+    enableEscape = true,
+  } = props;
+  return (
+    <div className="static-modal">
+      <Modal
+        show={show}
+        onHide={() => proceed(false)}
+        backdrop={enableEscape ? true : "static"}
+        keyboard={enableEscape}
+      >
+        <Modal.Header>
+          <Modal.Title>{title}</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>{confirmation}</Modal.Body>
+        <Modal.Footer>
+          {/* <Button onClick={() => proceed(false)}>{cancelLabel}</Button> */}
+          <Button
+            className="btn btn-primary save"
+            bsStyle="primary"
+            onClick={() => proceed(true)}
+          >
+            {proceedLabel}
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </div>
+  );
+};
+
+ModalContainer.propTypes = {
+  okLabbel: PropTypes.string,
+  cancelLabel: PropTypes.string,
+  title: PropTypes.string,
+  confirmation: PropTypes.object,
+  show: PropTypes.bool,
+  proceed: PropTypes.func, // called when ok button is clicked.
+  enableEscape: PropTypes.bool,
+  proceedLabel: PropTypes.any,
+};
+
+export default ModalContainer;
