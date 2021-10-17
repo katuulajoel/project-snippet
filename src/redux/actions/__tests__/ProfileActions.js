f65d87ce9efc47a6f83f517473dbe18a8331fdb9
sumohammed <sumohammed@hotmail.com>
2021-10-17 13:22:06 +0000

    Added ProfileActions tests

---
+++ src/redux/actions/__tests__/ProfileActions.js
@@ -0,0 +1,66 @@
+import configureMockStore from "redux-mock-store";
+import thunk from "redux-thunk";
+import axios from "axios";
+import * as actions from "../ProfileActions";
+
+const middlewares = [thunk];
+const mockStore = configureMockStore(middlewares);
+const store = mockStore({});
+
+jest.mock("axios", () => {
+  return {
+    interceptors: {
+      request: { use: jest.fn(), eject: jest.fn() },
+      response: { use: jest.fn(), eject: jest.fn() },
+    },
+    defaults: { xsrfCookieName: "csrftoken" },
+    post: jest.fn(),
+    get: jest.fn(),
+    patch: jest.fn(),
+  };
+});
+
+describe("Profile actions tests", () => {
+  beforeEach(() => {
+    axios.post.mockClear();
+    jest.clearAllMocks();
+    store.clearActions();
+  });
+
+  afterEach(() => {
+    store.clearActions();
+    jest.clearAllMocks();
+  });
+
+  it("should retrieve user settings", async () => {
+    axios.get.mockReturnValue(Promise.resolve({ data: {} }));
+    const expectedActions = [
+      {
+        type: "SET_USER_PROFILE",
+        payload: {
+          settings: {},
+        },
+      },
+    ];
+
+    await store.dispatch(actions.getSettings());
+    const storeActions = await store.getActions();
+    expect(storeActions).toEqual(expectedActions);
+  });
+
+  it("should update user settings", async () => {
+    axios.patch.mockReturnValue(Promise.resolve({ data: {} }));
+    const expectedActions = [
+      {
+        type: "SET_USER_PROFILE",
+        payload: {
+          settings: {},
+        },
+      },
+    ];
+
+    await store.dispatch(actions.updateSettings());
+    const storeActions = await store.getActions();
+    expect(storeActions).toEqual(expectedActions);
+  });
+});
