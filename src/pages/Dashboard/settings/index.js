900797fc0da6056c59c964f70e86ca270d18a610
sumohammed <sumohammed@hotmail.com>
2021-10-19 11:14:29 +0000

    Added payments page

--- src/pages/Dashboard/settings/index.js
+++ src/pages/Dashboard/settings/index.js
@@ -4,16 +4,16 @@ import { Switch, Route } from "react-router-dom";
 import DashboardLayout from "../../../layouts/DashboardLayout";
 import CreateUser from "./Invite/CreateUser";
 import Invite from "./Invite/InviteUser";
-// import Invite from "./Invite";
 import PendingInvite from "./Invite/pendingInvites/PendingInvites";
+import Payment from "./Payment";
 import Privacy from "./Privacy";
-// import * as userActions from "../../../actions/UserActions";
 
 export default function Settings() {
   return (
     <DashboardLayout>
       <div className={`content-card settings-card settings-container clearfix`}>
         <Switch>
+          <Route exact path="/settings/payment" component={Payment} />
           <Route exact path="/settings/privacy" component={Privacy} />
           <Route exact path="/settings/invite" component={Invite} />
           <Route exact path="/settings/invite/create" component={CreateUser} />
