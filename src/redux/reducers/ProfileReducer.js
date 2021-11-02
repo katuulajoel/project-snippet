2ebb5f8d40c674f5478fe974c83839bd6bf4760c
sumohammed <sumohammed@hotmail.com>
2021-11-02 10:06:31 +0000

    Added case to handle SET_USER_SETTINGS

--- src/redux/reducers/ProfileReducer.js
+++ src/redux/reducers/ProfileReducer.js
@@ -4,6 +4,8 @@ function profile(state = {}, action) {
   switch (action.type) {
     case actionTypes.SET_USER_PROFILE:
       return { ...state, ...action.data };
+    case actionTypes.SET_USER_SETTINGS:
+      return { ...state, settings: { ...action.data } };
     default:
       return state;
   }
