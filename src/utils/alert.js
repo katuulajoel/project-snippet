eedecb606da01351c883d5c4b34313c205fcd1a9
sumohammed <sumohammed@hotmail.com>
2021-11-14 01:54:44 +0000

    Added alert utils

---
+++ src/utils/alert.js
@@ -0,0 +1,34 @@
+const Alert = (msg, success = true) => {
+  const body = document.querySelector("body");
+
+  let toastContainer = document.querySelector(".toast-container");
+  if (!toastContainer) {
+    toastContainer = document.createElement("div");
+    toastContainer.className = "toast-container";
+  }
+
+  let uuid = Date.now();
+  const div = document.createElement("div");
+
+  div.className = `alert alert-${success ? "success" : "danger"} toast-${uuid}`;
+  div.innerHTML = msg;
+
+  toastContainer.prepend(div);
+  body.prepend(toastContainer);
+
+  setTimeout(function () {
+    let now = document.querySelector(`.toast-${uuid}`);
+    // now.style.display = "none";
+    if (now) {
+      now.remove();
+    }
+
+    // Check if alerts are finished and remove toast container
+    if (!document.querySelector(".alert")) {
+      let toast = document.querySelector(".toast-container");
+      toast.remove();
+    }
+  }, 2500);
+};
+
+export default Alert;
