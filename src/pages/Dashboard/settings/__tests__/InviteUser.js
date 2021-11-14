d69a6e4ff88788766f38d0685c26a206e4dbf06d
sumohammed <sumohammed@hotmail.com>
2021-11-14 02:00:49 +0000

    Updated test

--- src/pages/Dashboard/settings/__tests__/InviteUser.js
+++ src/pages/Dashboard/settings/__tests__/InviteUser.js
@@ -1,13 +1,36 @@
 import React from "react";
 import { Provider } from "react-redux";
-import { cleanup, render, fireEvent } from "@testing-library/react";
+import { cleanup, render, fireEvent, waitFor } from "@testing-library/react";
 import { BrowserRouter as Router } from "react-router-dom";
 import InviteUser from "../Invite/InviteUser";
 import store from "../../../../redux/store";
+import axios from "axios";
 
 afterEach(cleanup);
 
+jest.mock("axios", () => {
+  return {
+    interceptors: {
+      request: { use: jest.fn(), eject: jest.fn() },
+      response: { use: jest.fn(), eject: jest.fn() },
+    },
+    defaults: { xsrfCookieName: "csrftoken" },
+    post: jest.fn(),
+    get: jest.fn(),
+    request: jest.fn(),
+  };
+});
+
 describe("InviteUser comp test", () => {
+  beforeEach(() => {
+    axios.post.mockClear();
+    jest.clearAllMocks();
+  });
+
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
   it("InviteUser component snapshot", () => {
     const { asFragment } = render(
       <Provider store={store}>
@@ -67,6 +90,11 @@ describe("InviteUser comp test", () => {
   });
 
   it("Check form submit", () => {
+    axios.request.mockReturnValue(
+      Promise.reject({
+        response: { data: { email: ["User email already exists"] } },
+      })
+    );
     const { queryByLabelText, getByText } = render(
       <Provider store={store}>
         <Router>
@@ -92,5 +120,43 @@ describe("InviteUser comp test", () => {
     expect(lastNameInput.value).toBe("1");
 
     fireEvent.click(getByText(/Send Invite/i));
+  });
+
+  it("Check form submit successfully", async () => {
+    // axios.post.mockReturnValue(Promise.resolve({}));
+    axios.request.mockReturnValue(Promise.resolve({ data: [] }));
+    const { queryByLabelText, getByText } = render(
+      <Provider store={store}>
+        <Router>
+          <InviteUser />
+        </Router>
+      </Provider>
+    );
+
+    const emailInput = queryByLabelText("email-input");
+    fireEvent.change(emailInput, { target: { value: "ms@gm.com" } });
+    expect(emailInput.value).toBe("ms@gm.com");
+
+    const firstNameInput = queryByLabelText("first_name-input");
+    fireEvent.change(firstNameInput, { target: { value: "Kwame" } });
+    expect(firstNameInput.value).toBe("Kwame");
+
+    const lastNameInput = queryByLabelText("last_name-input");
+    fireEvent.change(lastNameInput, { target: { value: "Mensah" } });
+    expect(lastNameInput.value).toBe("Mensah");
+
+    const userTypeInput = queryByLabelText("type-input");
+    fireEvent.change(userTypeInput, { target: { value: "1" } });
+    expect(userTypeInput.value).toBe("1");
+
+    fireEvent.click(getByText(/Send Invite/i));
+
+    await waitFor(
+      () =>
+        expect(getByText(/Invitation Successfully Sent/i)).toBeInTheDocument(),
+      {
+        timeout: 6000, // wait 3s
+      }
+    );
   });
 });
