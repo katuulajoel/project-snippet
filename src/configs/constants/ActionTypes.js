0c6e081614685d681e148ed9bd4754f8a35e2b8a
sumohammed <sumohammed@hotmail.com>
2021-11-14 01:58:00 +0000

    Added button action type

--- src/configs/constants/ActionTypes.js
+++ src/configs/constants/ActionTypes.js
@@ -122,3 +122,6 @@ export const DELETE_PENDING_INVITE = "DELETE_PENDING_INVITE";
 // Profile Actions
 export const SET_USER_PROFILE = "SET_USER_PROFILE";
 export const SET_USER_SETTINGS = "SET_USER_SETTINGS";
+
+// Buttons
+export const SET_BUTTON = "SET_BUTTON";
