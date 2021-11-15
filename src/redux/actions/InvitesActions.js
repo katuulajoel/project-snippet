812e797545ebab6907802ab10cca5972252455d4
sumohammed <sumohammed@hotmail.com>
2021-11-15 16:37:12 +0000

    Updated actions

--- src/redux/actions/InvitesActions.js
+++ src/redux/actions/InvitesActions.js
@@ -2,13 +2,27 @@ import axios from "axios";
 import * as actionTypes from "../../configs/constants/ActionTypes";
 import { ENDPOINT_INVITE, ENDPOINT_USERS } from "../../utils/api";
 import { success } from "../../utils/actions";
+import Alert from "../../utils/alert";
 
 // TODO:
 // - Work error handling
 // - Loading via button
 
 export function createUser(data) {
-  return axios.post(ENDPOINT_USERS, data);
+  return (dispatch, sideEffect = null) =>
+    axios
+      .post(ENDPOINT_USERS, data)
+      .then(function () {
+        dispatch(success(actionTypes.SET_BUTTON, false));
+        Alert("Client Successfully Created.");
+        sideEffect && sideEffect();
+      })
+      .catch(function (error) {
+        dispatch(success(actionTypes.SET_BUTTON, false));
+        if (error.response.data && error.response.data["email"]) {
+          Alert(error.response.data["email"][0], false);
+        }
+      });
 }
 
 export function getPendingInvites() {
@@ -48,9 +62,25 @@ export function deleteInvite(id) {
 export function invite(data, isPatch = null) {
   let request_method = isPatch ? "patch" : "post";
 
-  return axios.request({
-    url: `${ENDPOINT_INVITE}${request_method === "patch" ? data.id + "/" : ""}`,
-    method: request_method,
-    data,
-  });
+  return (dispatch, sideEffect = null) =>
+    axios
+      .request({
+        url: `${ENDPOINT_INVITE}${
+          request_method === "patch" ? data.id + "/" : ""
+        }`,
+        method: request_method,
+        data,
+      })
+      .then(function () {
+        Alert("Invitation Successfully Sent.");
+        sideEffect && sideEffect();
+        return dispatch(success(actionTypes.SET_BUTTON, false));
+      })
+      .catch(function (error) {
+        if (error.response.data && error.response.data["email"]) {
+          // Request made and server responded
+          Alert(error.response.data["email"][0], false);
+        }
+        dispatch(success(actionTypes.SET_BUTTON, false));
+      });
 }
