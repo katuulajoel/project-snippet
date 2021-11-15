818c8502bc710b8baaccc0ea934e46115bb85e48
sumohammed <sumohammed@hotmail.com>
2021-11-15 14:32:39 +0000

    Removed default of required prop value

--- src/components/Label.js
+++ src/components/Label.js
@@ -2,7 +2,7 @@ import React from "react";
 import PropTypes from "prop-types";
 import { FormGroup } from "reactstrap";
 
-const Label = ({ name, required = true, children }) => (
+const Label = ({ name, required, children }) => (
   <div className="row" aria-label="label">
     <div className="col-sm-12">
       <FormGroup>
