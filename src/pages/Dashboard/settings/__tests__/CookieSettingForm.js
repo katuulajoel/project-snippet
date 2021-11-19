cf24fa86764ec6a62d6c68f975dc3a4075b3a65f
sumohammed <sumohammed@hotmail.com>
2021-11-19 01:51:31 +0000

    Updated tests

--- src/pages/Dashboard/settings/__tests__/CookieSettingForm.js
+++ src/pages/Dashboard/settings/__tests__/CookieSettingForm.js
@@ -9,11 +9,40 @@ import { COOKIE_OPTIONS } from "../../../../components/utils/consent";
 afterEach(cleanup);
 
 describe("CookieSettingForm layout test", () => {
+  let settings = {
+    visibility: { location_details: 1, profile_visibility: 1, social_links: 1 },
+    switches: {
+      cookie_analytics_customization: true,
+      cookie_performance_functionality: true,
+      cookie_targeting_advertising: true,
+      daily_update_email: true,
+      direct_messages_email: true,
+      event_email: true,
+      friend_request_response_email: true,
+      join_team_request_response_email: true,
+      new_friend_request_email: true,
+      new_task_application_email: true,
+      new_task_email: true,
+      new_task_invitation_email: true,
+      new_task_progress_report_email: true,
+      new_team_invitation_email: true,
+      newsletter_email: true,
+      payment_request_email: true,
+      payment_update_email: true,
+      task_activity_update_email: true,
+      task_application_response_email: true,
+      task_invitation_response_email: false,
+      task_progress_report_reminder_email: false,
+      task_survey_reminder_email: true,
+      team_invitation_response_email: true,
+    },
+  };
+
   it("CookieSettingForm component snapshot", () => {
     const { asFragment } = render(
       <Provider store={store}>
         <Router>
-          <CookieSettingForm onChange={jest.fn} settings={{}} />
+          <CookieSettingForm onChange={jest.fn} />
         </Router>
       </Provider>
     );
@@ -25,7 +54,7 @@ describe("CookieSettingForm layout test", () => {
     const { getByLabelText } = render(
       <Provider store={store}>
         <Router>
-          <CookieSettingForm onChange={jest.fn} settings={{}} />
+          <CookieSettingForm onChange={jest.fn} settings={settings} />
         </Router>
       </Provider>
     );
--- src/redux/actions/__tests__/ProfileActions.js
+++ src/redux/actions/__tests__/ProfileActions.js
@@ -2,7 +2,6 @@ import configureMockStore from "redux-mock-store";
 import thunk from "redux-thunk";
 import axios from "axios";
 import * as actions from "../ProfileActions";
-import { SET_USER_SETTINGS } from "../../../configs/constants/ActionTypes";
 
 const middlewares = [thunk];
 const mockStore = configureMockStore(middlewares);
@@ -33,57 +32,44 @@ describe("Profile actions tests", () => {
     jest.clearAllMocks();
   });
 
-  it("should retrieve user settings", async () => {
-    axios.get.mockReturnValue(Promise.resolve({ data: {} }));
-    const expectedActions = [
-      {
-        type: SET_USER_SETTINGS,
-        data: {},
-      },
-    ];
-
-    await store.dispatch(actions.getSettings());
-    const storeActions = await store.getActions();
-    expect(storeActions).toEqual(expectedActions);
-  });
-
-  it("should update user settings", async () => {
-    axios.patch.mockReturnValue(Promise.resolve({ data: {} }));
-    const expectedActions = [
-      {
-        type: SET_USER_SETTINGS,
-        data: {},
-      },
-    ];
-
-    await store.dispatch(actions.updateSettings());
-    const storeActions = await store.getActions();
-    expect(storeActions).toEqual(expectedActions);
-  });
-
   it("should update auth user settings", async () => {
     axios.patch.mockReturnValue(Promise.resolve({ data: {} }));
-    const expectedActions = [];
+    const expectedActions = [
+      {
+        data: false,
+        type: "SET_BUTTON",
+      },
+    ];
 
-    await store.dispatch(actions.updateAuthUser());
+    await actions.updateAuthUser()(store.dispatch);
     const storeActions = await store.getActions();
     expect(storeActions).toEqual(expectedActions);
   });
 
   it("should update user password", async () => {
     axios.post.mockReturnValue(Promise.resolve({ data: {} }));
-    const expectedActions = [];
+    const expectedActions = [
+      {
+        data: false,
+        type: "SET_BUTTON",
+      },
+    ];
 
-    await store.dispatch(actions.updatePassword());
+    await actions.updatePassword()(store.dispatch);
     const storeActions = await store.getActions();
     expect(storeActions).toEqual(expectedActions);
   });
 
   it("should update user account", async () => {
     axios.patch.mockReturnValue(Promise.resolve({ data: {} }));
-    const expectedActions = [];
+    const expectedActions = [
+      {
+        data: false,
+        type: "SET_BUTTON",
+      },
+    ];
 
-    await store.dispatch(actions.updateAccountInfo());
+    await actions.updateAccountInfo()(store.dispatch);
     const storeActions = await store.getActions();
     expect(storeActions).toEqual(expectedActions);
   });
