d2e6203839d4869e8203462253e0d427277358a0
Katuula <katuula@gmail.com>
2022-03-07 12:26:18 +0300

    remove code snippets

--- jest_config/setupTests.js
+++
@@ -1,9 +0,0 @@
-// learn more: https://github.com/testing-library/jest-dom
-import "@testing-library/jest-dom";
-
-import Enzyme from "enzyme";
-import Adapter from "@wojtekmaj/enzyme-adapter-react-17";
-
-Enzyme.configure({ adapter: new Adapter() });
-
-process.env.NODE_ENV = "dev";
